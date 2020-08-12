5434361e1376adc4dd8d11ef06ab5136c584ccec
Kamil Galant <kamil.galant@tieto.com>
2020-08-12 17:05:13 +0200

    improved saving paths and storing data in application settings

--- model/Utilities.py
+++ model/Utilities.py
@@ -2,6 +2,16 @@ import sys
 
 def formatPath(path):
     result = path.replace("file:///", "")
-    if sys.platform == "darwin":
+    if sys.platform == "darwin" and path != "":
         result = "/" + result
     return result
+
+def getOrEmptyStr(data, key):
+    if data.get(key) == None:
+        return ""
+    return data[key]
+
+def getOr(data, key, alternative):
+    if data.get(key) == None:
+        return alternative
+    return data[key]

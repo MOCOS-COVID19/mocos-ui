c7da0ebfa457a55eafcce79747d7c97b9c097670
Kamil Galant <kamil.galant@tieto.com>
2020-09-03 16:46:52 +0200

    fixed flake8 warnings

--- model/SimulationRunner.py
+++ model/SimulationRunner.py
@@ -58,12 +58,12 @@ class SimulationRunner(QThread):
     outputParamsDump = ""
     outputRunDumpPrefix = ""
     numOfThreads = 1
-    __getworkdir = None
-    __currentState = InitState.NONE
-    __currentProgress = 0
-    __process = None
-    __mutex = QMutex()
-    __isThreadStopped = False
+    _getworkdir = None
+    _currentState = InitState.NONE
+    _currentProgress = 0
+    _process = None
+    _mutex = QMutex()
+    _isThreadStopped = False
 
     printSimulationMsg = pyqtSignal(str, arguments=["msg"])
     notifyStateAndProgress = pyqtSignal(str, int, arguments=["state", "progress"])
@@ -72,7 +72,7 @@ class SimulationRunner(QThread):
 
     def __init__(self, getworkdir):
         QThread.__init__(self)
-        self.__getworkdir = getworkdir
+        self._getworkdir = getworkdir
         self.notifyStateAndProgress.connect(self.__saveCurrentProgress)
 
     def __del__(self):
@@ -80,30 +80,30 @@ class SimulationRunner(QThread):
 
     @pyqtProperty(bool, notify=isRunningChanged)
     def isRunning(self):
-        return (isinstance(self.__currentState, SimulationRunner.InitState) and
-                self.__currentState != SimulationRunner.InitState.NONE) or \
-            (isinstance(self.__currentState, SimulationRunner.SimulationState) and
-             self.__currentState == SimulationRunner.SimulationState.SIMULATION_ONGOING)
+        return (isinstance(self._currentState, SimulationRunner.InitState) and
+                self._currentState != SimulationRunner.InitState.NONE) or \
+            (isinstance(self._currentState, SimulationRunner.SimulationState) and
+             self._currentState == SimulationRunner.SimulationState.SIMULATION_ONGOING)
 
-    def __createCommand(self):
+    def _createCommand(self):
         cliname = "auto_advanced_cli.jl"
         cmd = ["julia", cliname]
         if self.outputParamsDump:
             cmd.append("--output-params-dump")
-            cmd.append(formatPath(self.__getworkdir() + "\\" + self.outputParamsDump))
+            cmd.append(formatPath(self._getworkdir() + "\\" + self.outputParamsDump))
         if self.outputDaily:
             cmd.append("--output-daily")
-            cmd.append(formatPath(self.__getworkdir() + "\\" + self.outputDaily))
+            cmd.append(formatPath(self._getworkdir() + "\\" + self.outputDaily))
         if self.outputSummary:
             cmd.append("--output-summary")
-            cmd.append(formatPath(self.__getworkdir() + "\\" + self.outputSummary))
+            cmd.append(formatPath(self._getworkdir() + "\\" + self.outputSummary))
         if self.outputRunDumpPrefix:
             cmd.append("--output-run-dump-prefix")
-            cmd.append(formatPath(self.__getworkdir() + "\\" + self.outputRunDumpPrefix))
+            cmd.append(formatPath(self._getworkdir() + "\\" + self.outputRunDumpPrefix))
         cmd.append(self.openedFilePath)
         return cmd
 
-    def __findSimulationProgressNotif(self, line):
+    def _findSimulationProgressNotif(self, line):
         PROGRESS_STR = "Progress:"
         if line.find(PROGRESS_STR) == -1:
             return None
@@ -112,99 +112,105 @@ class SimulationRunner(QThread):
         progressPercent = line[len(PROGRESS_STR): max].strip()
         return float(progressPercent) / 100.0
 
-    def __findInitStateNotif(self, line):
+    def _findInitStateNotif(self, line):
         if line.find("Info:") == -1:
             return (None, None)
         members = list(SimulationRunner.InitState)
-        nextIndex = members.index(self.__currentState) + 1
+        nextIndex = members.index(self._currentState) + 1
         if nextIndex >= len(members):
             nextIndex = len(members) - 1
         nextState = members[nextIndex]
         res = line.find(nextState.value)
         return (None, None) if res == -1 else (nextState, nextIndex/(len(members)-1))
 
-    def __isThreadStopped_Safe(self):
-        lk = QMutexLocker(self.__mutex)
-        return self.__isThreadStopped
+    def _isThreadStopped_Safe(self):
+        lk = QMutexLocker(self._mutex)  # noqa: F841
+        return self._isThreadStopped
 
-    def __setThreadStopped_Safe(self, isStopped):
-        lk = QMutexLocker(self.__mutex)
-        self.__isThreadStopped = isStopped
+    def _setThreadStopped_Safe(self, isStopped):
+        lk = QMutexLocker(self._mutex)  # noqa: F841
+        self._isThreadStopped = isStopped
 
     def __saveCurrentProgress(self, state, progress):
-        self.__currentProgress = progress
+        self._currentProgress = progress
 
     @pyqtSlot(result=str)
     def currentState(self):
-        return self.__currentState.toPrintable()
+        return self._currentState.toPrintable()
 
     @pyqtSlot(result=float)
     def currentProgress(self):
-        return self.__currentProgress
+        return self._currentProgress
 
     @staticmethod
     def isShellUsageRequired():
         return sys.platform != "darwin"
 
-    def run(self):
-        self.clearLog.emit()
-        if (not os.access(self.juliaCommand, os.X_OK) and not which(self.juliaCommand)) or self.openedFilePath == "":
+    def _processSimulationOutputMsg(self, line):
+        initState, initStateProgress = self._findInitStateNotif(line)
+        if initState is not None:
+            self._currentState = initState
+            self.notifyStateAndProgress.emit(self._currentState.toPrintable(), initStateProgress * 100)
             return
-        self.__currentState = SimulationRunner.InitState.INIT
-        self.notifyStateAndProgress.emit(self.__currentState.toPrintable(), 0)
-        self.isRunningChanged.emit()
-        dirname = ABS_PATH_TO_ADVANCED_CLI()
-        cmd = self.__createCommand()
-        self.printSimulationMsg.emit(dirname + '> ' + ' '.join(cmd) + '\n')
-        self.__process = subprocess.Popen(cmd, shell=SimulationRunner.isShellUsageRequired(),
-                                          cwd=dirname, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                                          env={**os.environ, "JULIA_PROJECT": dirname, "JULIA_NUM_THREADS": str(self.numOfThreads)},
-                                          encoding="utf-8")
-        if self.__isThreadStopped_Safe():
-            self.__currentState = SimulationRunner.InitState.NONE
+        simProgress = self._findSimulationProgressNotif(line)
+        if simProgress is not None:
+            self._currentState = SimulationRunner.SimulationState.SIMULATION_ONGOING
+            self.notifyStateAndProgress.emit(self._currentState.value, simProgress * 100)
             return
+        if line.find("ERROR") != -1:
+            self._currentState = SimulationRunner.SimulationState.ERROR
+            self.notifyStateAndProgress.emit(self._currentState.value, -1)
+
+    def _readSimulationOutput(self):
         communicates = queue.Queue()
-        readingOutputThread = threading.Thread(target=enqueueOutStream, args=(self.__process.stdout, communicates))
+        readingOutputThread = threading.Thread(target=enqueueOutStream, args=(self._process.stdout, communicates))
         readingOutputThread.daemon = True
         readingOutputThread.start()
         while True:
-            if self.__isThreadStopped_Safe():
+            if self._isThreadStopped_Safe():
                 break
-            if self.__process.poll() is not None:
+            if self._process.poll() is not None:
                 break
             try:
                 line = communicates.get(timeout=.1)
             except queue.Empty:
                 continue
             self.printSimulationMsg.emit(line)
-            initState, initStateProgress = self.__findInitStateNotif(line)
-            if initState is not None:
-                self.__currentState = initState
-                self.notifyStateAndProgress.emit(self.__currentState.toPrintable(), initStateProgress * 100)
-                continue
-            simProgress = self.__findSimulationProgressNotif(line)
-            if simProgress is not None:
-                self.__currentState = SimulationRunner.SimulationState.SIMULATION_ONGOING
-                self.notifyStateAndProgress.emit(self.__currentState.value, simProgress * 100)
-                continue
-            if line.find("ERROR") != -1:
-                self.__currentState = SimulationRunner.SimulationState.ERROR
-                self.notifyStateAndProgress.emit(self.__currentState.value, -1)
-        if self.__isThreadStopped_Safe():
-            self.__currentState = SimulationRunner.SimulationState.STOPPED
-            self.__setThreadStopped_Safe(False)
-        elif self.__currentState != SimulationRunner.SimulationState.ERROR:
-            self.__currentState = SimulationRunner.SimulationState.DONE
-        self.notifyStateAndProgress.emit(self.__currentState.value, -1)
+            self._processSimulationOutputMsg(line)
+
+    def run(self):
+        self.clearLog.emit()
+        if (not os.access(self.juliaCommand, os.X_OK) and not which(self.juliaCommand)) or self.openedFilePath == "":
+            return
+        self._currentState = SimulationRunner.InitState.INIT
+        self.notifyStateAndProgress.emit(self._currentState.toPrintable(), 0)
+        self.isRunningChanged.emit()
+        dirname = ABS_PATH_TO_ADVANCED_CLI()
+        cmd = self._createCommand()
+        self.printSimulationMsg.emit(dirname + '> ' + ' '.join(cmd) + '\n')
+        self._process = subprocess.Popen(cmd, shell=SimulationRunner.isShellUsageRequired(),
+                                         cwd=dirname, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                                         env={**os.environ, "JULIA_PROJECT": dirname, "JULIA_NUM_THREADS": str(self.numOfThreads)},
+                                         encoding="utf-8")
+        if self._isThreadStopped_Safe():
+            self._currentState = SimulationRunner.InitState.NONE
+            return
+        self._readSimulationOutput()
+        if self._isThreadStopped_Safe():
+            self._currentState = SimulationRunner.SimulationState.STOPPED
+            self._setThreadStopped_Safe(False)
+        elif self._currentState != SimulationRunner.SimulationState.ERROR:
+            self._currentState = SimulationRunner.SimulationState.DONE
+        self.notifyStateAndProgress.emit(self._currentState.value, -1)
         self.isRunningChanged.emit()
         self.clean()
 
     def stop(self):
-        if self.__process is not None:
-            self.__setThreadStopped_Safe(True)
+        if self._process is not None:
+            self._setThreadStopped_Safe(True)
             self.wait()
-            self.__process.kill()
-            self.__process = None
+            self._process.kill()
+            self._process = None
 
     def clean(self):
         if self.openedFilePath.find(formatPath(tempfile.gettempdir())) != -1:

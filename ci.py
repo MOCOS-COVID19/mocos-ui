2facb6ae91904fe67b701a3ba01db86c86d0778c
glowacki <mateusz.glowacki@tieto.com>
2020-09-29 17:08:40 +0200

    added support for pyinstaller on osx and introduce of ci.py

---
+++ ci.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import sys
+import os
+import argparse
+import subprocess
+
+
+def run(command):
+    cmd = list(command.split(" "))
+    out = subprocess.run(cmd, stdout=sys.stdout, stderr=sys.stderr)
+    if out.returncode != 0:
+        raise Exception("non 0 returncode")
+
+
+def deps():
+    run("pip install -U pip setuptools wheel")
+    run("pip install -r requirements.txt")
+    run("pip install -r requirements-ci.txt")
+
+
+def build():
+    run("pyinstaller mocos-gui.py")
+    run("cp -r views dist/mocos-gui/")
+    os.chdir("./dist")
+    if (os.environ['HOME'] == "windows"):
+        run("7z a -tzip mocos-gui-win-amd64.zip mocos-gui/")
+    if (os.environ['HOME'] == "osx"):
+        run("zip -r mocos-gui-osx-amd64.zip mocos-gui/")
+    else:
+        run("zip -r mocos-gui-linux-amd64.zip mocos-gui/")
+
+
+def main():
+    parser = argparse.ArgumentParser(description='ci script is designed to run on travis')
+    parser.add_argument('--deps', action='store_true', default=False,
+                        help='install dependencies')
+    parser.add_argument('--build', action='store_true', default=False,
+                        help='build pacakge using py-installer')
+
+    args = parser.parse_args()
+    if args.deps:
+        deps()
+    elif args.build:
+        build()
+    else:
+        print("pass at least one argument!")
+
+
+if __name__ == "__main__":
+    main()
